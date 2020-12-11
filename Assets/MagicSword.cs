6c15c19fbba6504ebf9cc1b5dd4f1d1e780ad21a
ryokyo1227 <72607177+ryokyo1227@users.noreply.github.com>
2020-12-11 21:00:01 +0900

    弾を撃てるようにした

---
+++ Assets/MagicSword.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicSword : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody.AddForce(Vector3.forward * 300);
+        Destroy(this.gameObject, 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public GameObject MagicSword;
+    public GameObject SwordPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if(transform.position.y<3.9f)
+                {
+                transform.position += new Vector3(0, 0.2f, 0);
+            }
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (transform.position.y > -2.4f)
+            {
+                transform.position -= new Vector3(0, 0.2f, 0);
+            }
+          
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if(transform.position.x < 4.6f)
+            {
+                transform.position += new Vector3(0.2f, 0, 0);
+            }
+        
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (transform.position.x > -4.6f)
+            {
+                transform.position -= new Vector3(0.2f, 0, 0);
+            }
+       
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Instantiate(MagicSword, SwordPosition.transform.position,MagicSword.transform.rotation);
+        }
+    }
+}
