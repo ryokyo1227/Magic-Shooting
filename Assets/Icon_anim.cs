77c4002b40f4d8dc689bdcf7e7a87b734e8e48b8
ryokyo1227 <72607177+ryokyo1227@users.noreply.github.com>
2021-04-02 21:08:14 +0900

    アイコンアニメ追加

---
+++ Assets/Icon_anim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Icon_anim : MonoBehaviour
+{
+    public Image Sh_image;
+    public Sprite[] Sh_sprite;
+    int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+        InvokeRepeating("Anim", 0, 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Anim()
+    {
+        Sh_image.sprite = Sh_sprite[index];
+        index++;
+        if (index >= Sh_sprite.Length)
+        {
+            index = 0;
+        }
+    }
+}
